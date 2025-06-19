6f5b137cf20d5b0063b74e6ba46f5f9f02a16e65
WorriedBottle <14600912+worriedbottle@user.noreply.gitee.com>
2025-06-19 15:08:15 +0800

    ✨ feat: 添加云台电机开环控制（未同时获取编码值）

--- Gimbal_Control/Laser_Omni/Core/Src/main.c
+++ Gimbal_Control/Laser_Omni/Core/Src/main.c
@@ -19,12 +19,14 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "i2c.h"
+#include "tim.h"
 #include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "as5600.h"
+#include <math.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -34,7 +36,8 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define _constrain(amt, low, high) ((amt) < (low) ? (low) : ((amt) > (high) ? (high) : (amt)))
+#define PI 3.1415926
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -45,18 +48,24 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
+float voltage_power_supply = 12.6;
+float shaft_angle=0, open_loop_timestamp=0;
+float zero_electric_angle=0, Ualpha=0, Ubeta=0, Ua=0, Ub=0, Uc=0, dc_a=0, dc_b=0, dc_c=0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
+float velocity_openloop(float target_velocity);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+uint32_t micros() {
+    uint32_t ms = HAL_GetTick();                    // 毫秒部分
+    uint32_t us = (SysTick->LOAD - SysTick->VAL) / (SystemCoreClock / 1000000); // 微秒部分
+    return ms * 1000 + us;
+}
 /* USER CODE END 0 */
 
 /**
@@ -90,16 +99,21 @@ int main(void)
   MX_GPIO_Init();
   MX_I2C1_Init();
   MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-      Programe_Run();
-      HAL_Delay(500);
+//      Programe_Run();
+//      HAL_Delay(500);
+      velocity_openloop(30);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -147,6 +161,77 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+// 电角度求解
+float get_electrical_angle(float shaft_angle, int pole_pairs) {
+    return (shaft_angle * pole_pairs);
+}
+
+// 归一化角度到 [0,2PI]
+float normalize_angle(float angle){
+    float a = fmod(angle, 2*PI);   //取余运算可以用于归一化，列出特殊值例子算便知
+    return a >= 0 ? a : (a + 2*PI);
+    //三目运算符。格式：condition ? expr1 : expr2
+    //其中，condition 是要求值的条件表达式，如果条件成立，则返回 expr1 的值，否则返回 expr2 的值。可以将三目运算符视为 if-else 语句的简化形式。
+    //fmod 函数的余数的符号与除数相同。因此，当 angle 的值为负数时，余数的符号将与 _2PI 的符号相反。也就是说，如果 angle 的值小于 0 且 _2PI 的值为正数，则 fmod(angle, _2PI) 的余数将为负数。
+    //例如，当 angle 的值为 -PI/2，_2PI 的值为 2PI 时，fmod(angle, _2PI) 将返回一个负数。在这种情况下，可以通过将负数的余数加上 _2PI 来将角度归一化到 [0, 2PI] 的范围内，以确保角度的值始终为正数。
+}
+
+void set_pwm(float Ua, float Ub, float Uc) {
+
+    // 计算占空比
+    // 限制占空比从0到1
+    dc_a = _constrain(Ua / voltage_power_supply, 0.0f , 1.0f );
+    dc_b = _constrain(Ub / voltage_power_supply, 0.0f , 1.0f );
+    dc_c = _constrain(Uc / voltage_power_supply, 0.0f , 1.0f );
+
+    //写入PWM到PWM 0 1 2 通道
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, dc_a*255);
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, dc_b*255);
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, dc_c*255);
+//    ledcWrite(0, dc_a*255);
+//    ledcWrite(1, dc_b*255);
+//    ledcWrite(2, dc_c*255);
+}
+
+void set_phase_voltage(float Uq,float Ud, float angle_el) {
+    angle_el = normalize_angle(angle_el + zero_electric_angle);
+    // 帕克逆变换
+    Ualpha = -Uq * sin(angle_el);
+    Ubeta  =  Uq * cos(angle_el);
+
+    // 克拉克逆变换
+    Ua = Ualpha + voltage_power_supply/2;
+    Ub = (sqrt(3)*Ubeta-Ualpha)/2 + voltage_power_supply/2;
+    Uc = (-Ualpha-sqrt(3)*Ubeta)/2 + voltage_power_supply/2;
+    set_pwm(Ua, Ub, Uc);
+}
+
+//开环速度函数
+float velocity_openloop(float target_velocity){
+    unsigned long now_us = micros();  //获取从开启芯片以来的微秒数，它的精度是 4 微秒。 micros() 返回的是一个无符号长整型（unsigned long）的值
+
+    //计算当前每个Loop的运行时间间隔
+    float Ts = (now_us - open_loop_timestamp) * 1e-6f;
+
+    //由于 micros() 函数返回的时间戳会在大约 70 分钟之后重新开始计数，在由70分钟跳变到0时，TS会出现异常，因此需要进行修正。如果时间间隔小于等于零或大于 0.5 秒，则将其设置为一个较小的默认值，即 1e-3f
+    if(Ts <= 0 || Ts > 0.5f) Ts = 1e-3f;
+
+
+    // 通过乘以时间间隔和目标速度来计算需要转动的机械角度，存储在 shaft_angle 变量中。在此之前，还需要对轴角度进行归一化，以确保其值在 0 到 2π 之间。
+    shaft_angle = normalize_angle(shaft_angle + target_velocity*Ts);
+    //以目标速度为 10 rad/s 为例，如果时间间隔是 1 秒，则在每个循环中需要增加 10 * 1 = 10 弧度的角度变化量，才能使电机转动到目标速度。
+    //如果时间间隔是 0.1 秒，那么在每个循环中需要增加的角度变化量就是 10 * 0.1 = 1 弧度，才能实现相同的目标速度。因此，电机轴的转动角度取决于目标速度和时间间隔的乘积。
+
+    // 使用早前设置的voltage_power_supply的1/3作为Uq值，这个值会直接影响输出力矩
+    // 最大只能设置为Uq = voltage_power_supply/2，否则ua,ub,uc会超出供电电压限幅
+    float Uq = voltage_power_supply/3;
+
+    set_phase_voltage(Uq,  0, get_electrical_angle(shaft_angle, 7));
+
+    open_loop_timestamp = now_us;  //用于计算下一个时间间隔
+
+    return Uq;
+}
 
 /* USER CODE END 4 */
 
--- Gimbal_Control/Laser_Omni/Core/Src/usart.c
+++ Gimbal_Control/Laser_Omni/Core/Src/usart.c
@@ -25,6 +25,7 @@
 /* USER CODE END 0 */
 
 UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
 
 /* USART1 init function */
 
@@ -55,6 +56,35 @@ void MX_USART1_UART_Init(void)
   /* USER CODE END USART1_Init 2 */
 
 }
+/* USART2 init function */
+
+void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
 
 void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
 {
@@ -68,24 +98,53 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
     /* USART1 clock enable */
     __HAL_RCC_USART1_CLK_ENABLE();
 
-    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
     /**USART1 GPIO Configuration
-    PA9     ------> USART1_TX
-    PA10     ------> USART1_RX
+    PB6     ------> USART1_TX
+    PB7     ------> USART1_RX
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Pin = GPIO_PIN_6;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = GPIO_PIN_10;
+    GPIO_InitStruct.Pin = GPIO_PIN_7;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    __HAL_AFIO_REMAP_USART1_ENABLE();
 
   /* USER CODE BEGIN USART1_MspInit 1 */
 
   /* USER CODE END USART1_MspInit 1 */
+  }
+  else if(uartHandle->Instance==USART2)
+  {
+  /* USER CODE BEGIN USART2_MspInit 0 */
+
+  /* USER CODE END USART2_MspInit 0 */
+    /* USART2 clock enable */
+    __HAL_RCC_USART2_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_2;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN USART2_MspInit 1 */
+
+  /* USER CODE END USART2_MspInit 1 */
   }
 }
 
@@ -101,14 +160,32 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
     __HAL_RCC_USART1_CLK_DISABLE();
 
     /**USART1 GPIO Configuration
-    PA9     ------> USART1_TX
-    PA10     ------> USART1_RX
+    PB6     ------> USART1_TX
+    PB7     ------> USART1_RX
     */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9|GPIO_PIN_10);
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_6|GPIO_PIN_7);
 
   /* USER CODE BEGIN USART1_MspDeInit 1 */
 
   /* USER CODE END USART1_MspDeInit 1 */
+  }
+  else if(uartHandle->Instance==USART2)
+  {
+  /* USER CODE BEGIN USART2_MspDeInit 0 */
+
+  /* USER CODE END USART2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART2_CLK_DISABLE();
+
+    /**USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2|GPIO_PIN_3);
+
+  /* USER CODE BEGIN USART2_MspDeInit 1 */
+
+  /* USER CODE END USART2_MspDeInit 1 */
   }
 }
 
